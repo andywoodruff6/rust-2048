0ef4be5bacd101a90975b511420e958ed3debf56
TheNewBadger <83123367+andywoodruff6@users.noreply.github.com>
2024-01-19 16:11:41 -0600

    Add HelloPlugin and implement greeting functionality

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,55 @@
 use bevy::prelude::*;
 
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
+
+#[derive(Resource)]
+struct GreetTimer(Timer);
+
 fn main() {
-    App::new().run();
+    App::new()
+        .add_plugins((DefaultPlugins, HelloPlugin))
+        .run();
+}
+
+pub struct HelloPlugin;
+
+impl Plugin for HelloPlugin {
+    fn build(&self, app: &mut App) {
+        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
+            .add_systems(Startup, add_people)
+            .add_systems(Update, greet_people);
+    }
+}
+
+
+
+fn hello_world() {
+    println!("hello world");
+}
+
+fn add_people(mut commands: Commands) {
+    commands.spawn((Person, Name("Elaina Proctor".to_string())));
+    commands.spawn((Person, Name("Renzo Hume".to_string())));
+    commands.spawn((Person, Name("Zayna Nieves".to_string())));
+}
+
+fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+    if timer.0.tick(time.delta()).just_finished() {
+        for name in query.iter() {
+            println!("hello {}!", name.0);
+        }
+    }
+}
+
+fn update_people(mut query: Query<&mut Name, With<Person>>) {
+    for mut name in &mut query {
+        if name.0 == "Elaina Proctor" {
+            name.0 = "Elaina Hume".to_string();
+            break; // We don’t need to change any other names
+        }
+    }
 }
